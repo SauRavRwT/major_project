17eadf9132cc0f3311e2d72d0750a48c1ac7a9bd
Saurav Rawat <90666710+SauRavRwT@users.noreply.github.com>
2022-04-23 13:46:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -64,14 +64,58 @@ const saveMessages = (name, emailid, msgContent) => {
   });
 };
 function showAlert() {
-  alert ("Your Feedback is Submitted");
+  alert("Your Feedback is Submitted");
 }
 const getElementVal = (id) => {
   return document.getElementById(id).value;
 };
 
-/* typewritter */ 
-var TxtType = function(el, toRotate, period) {
+/* Firebase Authorisation */
+const auth = firebase.auth();
+
+//signup function
+function signUp() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+
+  const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+  //
+  promise.catch(e => alert(e.message));
+  alert("Account Created");
+}
+
+//signIN function
+function signIn() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+  promise.catch(e => alert(e.message));
+
+}
+
+
+//signOut
+
+function signOut() {
+  auth.signOut();
+  alert("You'r SignOut");
+}
+
+//active user to homepage
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    var email = user.email;
+    alert(email);
+
+    // window.location = "./Web Collection.html";
+
+  } else {
+    // alert("Error No user is there")
+  }
+})
+
+/* typewritter */
+var TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
@@ -81,17 +125,17 @@ var TxtType = function(el, toRotate, period) {
   this.isDeleting = false;
 };
 
-TxtType.prototype.tick = function() {
+TxtType.prototype.tick = function () {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
 
   if (this.isDeleting) {
-  this.txt = fullTxt.substring(0, this.txt.length - 1);
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
-  this.txt = fullTxt.substring(0, this.txt.length + 1);
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
   var that = this;
   var delta = 200 - Math.random() * 100;
@@ -99,27 +143,27 @@ TxtType.prototype.tick = function() {
   if (this.isDeleting) { delta /= 2; }
 
   if (!this.isDeleting && this.txt === fullTxt) {
-  delta = this.period;
-  this.isDeleting = true;
+    delta = this.period;
+    this.isDeleting = true;
   } else if (this.isDeleting && this.txt === '') {
-  this.isDeleting = false;
-  this.loopNum++;
-  delta = 500;
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
   }
 
-  setTimeout(function() {
-  that.tick();
+  setTimeout(function () {
+    that.tick();
   }, delta);
 };
 
-window.onload = function() {
+window.onload = function () {
   var elements = document.getElementsByClassName('typewrite');
-  for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-type');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtType(elements[i], JSON.parse(toRotate), period);
-      }
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
   }
   // INJECT CSS
   var css = document.createElement("style");
@@ -127,8 +171,3 @@ window.onload = function() {
   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff} ";
   document.body.appendChild(css);
 };
-
-// navbar
-const nav =document.getElementById('nav');
-const toggle =document.getElementById('toggle')
-toggle.addEventListener('click', () => nav.classList.toggle('active'))
